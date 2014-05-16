e0f256f4d0e3a733ea272f4ce3f975a6d6121cc3
zross <zev@zevross.com>
2014-05-16 13:19:16 -0400

    got flags working

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3,20 +3,30 @@
 /* Controllers DON'T MANIPULATE DOM WITH CONTROLLER ONLY WITH DIRECTIVE
 MIGHT WANT TO USE NG-CLOAK SO WE DON'T SEE FLASHES OF UN PARSED DATA
 OR PUT ANGULAR SCRIPT IN HEAD*/
-
-myApp.controller('DemoController', ["$scope", "$http",'$q',
+var blah;
+myApp.controller('DemoController', ["$scope", "$http", '$q',
     function($scope, $http, $q) {
 
-    	 $scope.orderByField = 'fifarank';
-  		$scope.reverseSort = false;
+        $scope.orderByField = 'fifarank';
+        $scope.reverseSort = false;
         $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, leafletEvent) {
-            //countryMouseover(leafletEvent);
+            countryMouseover(leafletEvent);
         });
 
         $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
+            console.log(featureSelected)
             countryClick(featureSelected, leafletEvent);
         });
 
+    function countryMouseover(leafletEvent) {
+                var layer = leafletEvent.target;
+                layer.setStyle({
+                    weight: 2,
+                    color: '#666',
+                    fillColor: 'white'
+                });
+                layer.bringToFront();
+            }
 
         angular.extend($scope, {
             center: {
@@ -36,89 +46,81 @@ myApp.controller('DemoController', ["$scope", "$http",'$q',
 
 
         function getColorFootball(d) {
-  
-        	if (d){
 
-        		d=d['fifarank']
-        	}
-            return d > 35 ? ['#800026',1] :
-                   d > 30  ? ['#BD0026',1] :
-                   d > 25  ? ['#E31A1C',1] :
-                   d > 20  ? ['#FC4E2A',1] :
-                   d > 15   ? ['#FD8D3C',1] :
-                   d > 10   ? ['#FEB24C',1] :
-                   d > 5   ? ['#FED976',1] :
-                              ['grey',0];
+            if (d) {
+                d = d['fifarank']
+            }
+            return d > 35 ? ['#800026', 1] :
+                d > 25 ? ['#BD0026', 1] :
+                d > 20 ? ['#E31A1C', 1] :
+                d > 15 ? ['#FC4E2A', 1] :
+                d > 10 ? ['#FD8D3C', 1] :
+                d > 5 ? ['#FEB24C', 1] :
+                d > 0 ? ['#FED976', 1] : ['grey', 0];
         }
 
 
 
 
         function style(feature) {
-  			var vals = getColorFootball($scope.football[feature.properties.ISO3])
+            var vals = getColorFootball($scope.football[feature.properties.ISO3])
             return {
                 fillColor: vals[0],
                 radius: 8,
-                  color: "#000",
-                  weight: 1,
-                  opacity: vals[1],
-                  fillOpacity: vals[1]
+                color: "#000",
+                weight: 1,
+                opacity: vals[1],
+                fillOpacity: vals[1]
             };
         }
 
-        $http.get("data/football.json").then(function(result) {
-                var data=result.data
-                var football = {};
-                for (var i = 0; i < data.length; i++) {
-                    var country = data[i];
-                    football[country['alpha-3']] = country;
+        $scope.football = {};
+        $http.get("data/football.json").success(function(data, status) {
+            var tempFootballJson = {};
 
+            for (var i = 0; i < data.length; i++) {
+                var country = data[i];
+                tempFootballJson[country['alpha-3']] = country;
+            }
+
+            //then set on scope
+            $scope.football = tempFootballJson;
+    
+
+        });
+
+        $scope.countries = {};
+        $http.get("data/all.json").success(function(data, status) {
+
+           var tmpcountries = {};
+                for (var i=0; i< data.length; i++) {
+                    var country = data[i];
+                    tmpcountries[country['alpha-3']] = country;
                 }
-                return $scope.football = football
+
+            //then set on scope
+            
+            $scope.countries = tmpcountries;
+            blah=tmpcountries;
+            console.log($scope.countries)
+
         });
 
 
+        // http://thematicmapping.org/downloads/world_borders.php
+        // qgis to do centroids, move US, save as geojson
+        $http.get("data/countriespt2.geojson").success(function(data, status) {
+ 
 
-            // http://thematicmapping.org/downloads/world_borders.php
-            // qgis to do centroids, move US, save as geojson
-            $http.get("data/countriespt2.geojson").success(function(data, status) {
-                angular.extend($scope, {
-                    geojson: {
-                        data: data,
-                        style: style
-                        // resetStyleOnMouseout: true
-                    }
-                });
+            angular.extend($scope, {
+                geojson: {
+                    data: data,
+                    style: style,
+                    resetStyleOnMouseout: true
+                }
             });
-// myAppServices.factory('getLocalTopoJSONSrvc', function($q, $http) {
-//   return {
-
-//     getLocalTopoJSON: function() {
-
-//       //return $http.get("data/townshipQ5000_id_1e-8.json")      
-//       return $http.get("data/county.topojson").then(function(response) {
-//         if (typeof response.data === 'object') {
-
-//           var geoJsonObject = topojson.feature(response.data, response.data.objects.county)
-
-//           return geoJsonObject;
-//         } else {
-//             // invalid response
-//             return $q.reject(response.data);
-//         }
-
-//       }, function(response) {
-//         // something went wrong
-//         return $q.reject(response.data);
-//       }); 
-        
-//       // map.data.addGeoJson(geoJsonObject);               
-
-//     }// end getGeoJSON function
-
-
-//   };
-// });
+        });
+    
 
 
 
