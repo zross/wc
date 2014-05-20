3d6037aa87814111b9822cdbf561e34be98a3a34
zross <zev@zevross.com>
2014-05-20 12:48:47 -0400

    filtering all working

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -9,3 +9,19 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }]);
+
+  myApp.directive('fix', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(value) {
+                console.log(value);
+                if ( value === null ) {
+                    value = '';
+                }
+                console.log(value);
+                return value;
+            });
+        }
+    };
+});
